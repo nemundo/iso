ee7a3af8c27b1e03eec47bf9f7602e9e90a5e55c
unknown <urs.lang@gmail.com>
2020-09-24 17:51:13 +0200

    .

--- src/Parameter/CountryParameter.php
+++ src/Parameter/CountryParameter.php
@@ -1,8 +1,26 @@
 <?php
+
 namespace Nemundo\Iso\Parameter;
+
+use Nemundo\Iso\Data\Country\CountryReader;
 use Nemundo\Web\Parameter\AbstractUrlParameter;
-class CountryParameter extends AbstractUrlParameter {
-protected function loadParameter() {
-$this->parameterName = 'country';
-}
+
+class CountryParameter extends AbstractUrlParameter
+{
+
+    protected function loadParameter()
+    {
+
+        $this->parameterName = 'country';
+
+    }
+
+
+    public function getCountryRow()
+    {
+
+        return (new CountryReader())->getRowById($this->getValue());
+
+    }
+
 }
