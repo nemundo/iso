53fddb0f18c82363574812c9552fd927d16425ce
unknown <urs.lang@gmail.com>
2020-11-08 21:03:04 +0100

    .

--- src/Data/IsoModelCollection.php
+++ src/Data/IsoModelCollection.php
@@ -1,7 +1,7 @@
 <?php
 namespace Nemundo\Iso\Data;
 use Nemundo\Model\Collection\AbstractModelCollection;
-class IsoCollection extends AbstractModelCollection {
+class IsoModelCollection extends AbstractModelCollection {
 protected function loadCollection() {
 $this->addModel(new \Nemundo\Iso\Data\Country\CountryModel());
 $this->addModel(new \Nemundo\Iso\Data\Region\RegionModel());
--- src/Install/IsoInstall.php
+++ src/Install/IsoInstall.php
@@ -2,13 +2,13 @@
 
 namespace Nemundo\Iso\Install;
 
-use Nemundo\App\Script\Setup\ScriptSetup;
-use Nemundo\Iso\Script\IsoImportScript;
-use Nemundo\Project\Install\AbstractInstall;
-use Nemundo\Model\Setup\ModelCollectionSetup;
-use Nemundo\Iso\Data\IsoCollection;
-use Nemundo\Iso\Application\IsoApplication;
 use Nemundo\App\Application\Setup\ApplicationSetup;
+use Nemundo\App\Script\Setup\ScriptSetup;
+use Nemundo\Iso\Application\IsoApplication;
+use Nemundo\Iso\Data\IsoModelCollection;
+use Nemundo\Iso\Script\IsoImportScript;
+use Nemundo\Model\Setup\ModelCollectionSetup;
+use Nemundo\Project\Install\AbstractInstall;
 
 class IsoInstall extends AbstractInstall
 {
@@ -17,16 +17,11 @@ class IsoInstall extends AbstractInstall
 
 
         (new ApplicationSetup())->addApplication(new IsoApplication());
-        (new ModelCollectionSetup())->addCollection(new IsoCollection());
+        (new ModelCollectionSetup())->addCollection(new IsoModelCollection());
 
 
         (new ScriptSetup())
             ->addScript(new IsoImportScript());
-
-
-
-
-
 
 
     }
